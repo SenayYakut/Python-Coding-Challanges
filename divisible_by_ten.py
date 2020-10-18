db0a06e0e1f4c6d44455911217b92f8389299c83
Senay Yakut <senaykt@gmail.com>
2020-10-17 21:25:59 -0700

    Add solutions for difficult python code challanges

--- divisible_by_ten.py
+++ divisible_by_ten.py
@@ -15,3 +15,16 @@ print(divisible_by_ten(20))
 # should print True
 print(divisible_by_ten(25))
 # should print False
+
+#Write your function here
+def divisible_by_ten(nums):
+  counter = 0
+  for num in nums:
+    if num % 10 == 0:
+      counter += 1
+  return counter  
+
+
+
+#Uncomment the line below when your function is done
+print(divisible_by_ten([20, 25, 30, 35, 40]))
